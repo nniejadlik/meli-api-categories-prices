1cb15385a7362fe5b2e162c5bde664481c167d27
Nicolás Niejadlik <nicolas@Nicolass-MacBook-Pro.local>
2017-11-23 00:49:02 -0300

    Fix test

--- categories-prices_test.go
+++ categories-prices_test.go
@@ -6,83 +6,56 @@ import (
 	"math/rand"
 )
 
+var Categories = [8]string {"MLA1234", "MLA1235", "MLA123", "12345", "MLA109291", "MLA5725", "MLA4711", "MLA6520"}
 
 func TestGetPrices(t *testing.T){
-	fmt.Print("\nGetPrices\n")
 
-	categories := map[string]map[string]PriceType{
-		"MLA1234":{"max":0.0,"suggested":0.0,"min":0.0},
-		"MLA1235":{"max":0.0,"suggested":0.0,"min":0.0},
-		"MLA123":{"max":0.0,"suggested":0.0,"min":0.0},
-		"12345":{"max":0.0,"suggested":0.0,"min":0.0},
-		"MLA109291":{"max":50000,"suggested":25000.5,"min":1},
-		"MLA5725":{"max":1e+10,"suggested":5e+09,"min":1},
-		"MLA4711":{"max":1.1111111e+08,"suggested":5.5555556e+07,"min":1},
-		"MLA6520":{"max":1e+10,"suggested":5e+09,"min":1},
+	for _,i:= range Categories{
+		prices := GetPrices(i)
+
+		if !(prices["max"] >= 0){
+			t.Error(fmt.Sprintf("The maximum price was expected for category %s greater than zero, but instead got %v", i, prices["max"] ))
 		}
 
-	for k,i:= range categories{
-		prices := GetPrices(k)
-
-		if prices["max"] != i["max"]{
-			t.Error(fmt.Sprintf("Expected the max price %v for category %s  but instead got %v", i["max"], k, prices["max"] ))
+		if !(prices["min"] >= 0){
+			t.Error(fmt.Sprintf("The minimum price was expected for category %s greater than zero, but instead got %v", i, prices["min"] ))
 		}
 
-		if prices["min"] != i["min"]{
-			t.Error(fmt.Sprintf("Expected the min price %v for category %s  but instead got %v", i["min"], k, prices["min"] ))
-		}
-
-		if prices["suggested"] != i["suggested"]{
-			t.Error(fmt.Sprintf("Expected the suggested price %v for category %s  but instead got %v", i["suggested"], k, prices["suggested"] ))
+		if !(prices["suggested"] >= 0){
+			t.Error(fmt.Sprintf("The suggested price was expected for category %s greater than zero, but instead got %v", i, prices["suggested"] ))
 		}
 	}
 }
 
-
 func TestGetData(t *testing.T) {
-	fmt.Print("\ngetData\n")
 
-	categories := []struct {
-		id string
-        data struct {
-        	price PriceType
-			order string
-		}
-    }{
-    	{"MLA1234", {0.0, "price_asc"}},
-		{"MLA1235",{0.0, "price_desc"}},
-		{"MLA123",{0.0, "price_asc"}},
-		{"12345",{0.0, "price_desc"}},
-		{"MLA109291",{1, "price_asc"}},
-		{"MLA5725",{1e+10, "price_desc"}},
-		{"MLA4711",{1, "price_asc"}},
-		{"MLA6520",{1e+10, "price_desc"}},
-	}
+	order := map[string]string {"MLA1234":"price_asc", "MLA1235":"price_desc", "MLA123":"price_asc", "12345":"price_desc", "MLA109291":"price_asc", "MLA5725":"price_desc", "MLA4711":"price_asc", "MLA6520":"price_desc"}
 
-
-	for category := range categories {
-		price := getData(category["id"], category["data"]["order"])
-		if price != PriceType(category["data"]["price"]){
-			t.Error(fmt.Sprintf("Expected the price %v for category %s  but instead got %v", category["data"]["price"], category["id"], price ))
+	for _, category := range Categories {
+		price := getData(category, order[category])
+		if !(price >= 0){
+			t.Error(fmt.Sprintf("The price was expected greater than zero, but instead got %v", price ))
 		}
 	}
 }
 
 
 func TestGetMaxPrice(t *testing.T){
-	fmt.Print("\ngetMaxPrice\n")
 
 	var prices = make(chan PriceType,2)
-	categories := [8]string{"MLA1234","MLA1235","MLA123","12345","MLA109291","MLA5725","MLA4711","MLA6520"}
 
-	for _,i:= range categories{
+
+	for _,i:= range Categories{
 		go getMaxPrice(i,prices)
 	}
 
 	count := 0
 	for i:= range prices{
 		count++
-		fmt.Println(i)
+
+		if !(i >= 0){
+			t.Error(fmt.Sprintf("The maximum price was expected greater than zero, but instead got %v", i ))
+		}
 
 		if count == 8{
 			break
@@ -93,7 +66,6 @@ func TestGetMaxPrice(t *testing.T){
 
 
 func TestGetMinPrice(t *testing.T){
-	fmt.Print("\ngetMinPrice\n")
 	var prices = make(chan PriceType,2)
 	categories := [8]string{"MLA1234","MLA1235","MLA123","12345","MLA109291","MLA5725","MLA4711","MLA6520"}
 
@@ -104,7 +76,9 @@ func TestGetMinPrice(t *testing.T){
 	count := 0
 	for i:= range prices{
 		count++
-		fmt.Println(i)
+		if !(i >= 0){
+			t.Error(fmt.Sprintf("The minimum price was expected greater than zero, but instead got %v", i ))
+		}
 
 		if count == 8{
 			break
@@ -115,47 +89,64 @@ func TestGetMinPrice(t *testing.T){
 
 
 func TestGetsuggestedPrice(t *testing.T){
-	fmt.Print("\ngetsuggestedPrice\n")
 
 	for i:=0; i<10; i++{
 		var x,y PriceType = PriceType(rand.Float32()), PriceType(rand.Float32())
 		price := getSuggestedPrice(x,y)
-		fmt.Printf("Value between %v and %v is %v\n",x,y, price)
+
+		if !(price >= 0){
+			t.Error(fmt.Sprintf("The suggested price was expected greater than zero, but instead got %v", price ))
+		}
 	}
 }
 
 
 func TestFormatPrice(t *testing.T){
-	fmt.Print("\nformatPrice\n")
 	prices := [8]PriceType{0.0,1.5,6,98.98,9090,7,98.098,76}
 
 	for _,i:= range prices{
 		price := formatPrice(i)
-		fmt.Printf("The price %v formated is: %v\n",i,price)
+
+		if !(price >= 0){
+			t.Error(fmt.Sprintf("The price was expected greater than zero, but instead got %v", price ))
+		}
 	}
 }
 
 
-func BenchmarkGetPrices(b *testing.B){
+func benchmarkGetPrices(cant int, b *testing.B){
 	category := "MLA6520"
 
-	b.N = 500
-
-	fmt.Print("\nBenchmarGetPrices\n")
-
-	for i := 0; i < b.N; i++ {
-		prices := GetPrices(category)
-		fmt.Printf("The prices for %s is %v\n",category,prices)
-	}
-
-	//Parallel
-	fmt.Print("\nBenchmarGetPrices - Run parallel\n")
+	b.N = cant
 
 	b.RunParallel(
 		func(pb *testing.PB) {
 			for pb.Next() {
-				prices := GetPrices(category)
-				fmt.Printf("[Parallel] The prices for %s is %v\n",category,prices)
+				GetPrices(category)
 			}
 		})
+}
+
+func BenchmarkGetPrices1(b *testing.B) {
+	benchmarkGetPrices(1, b)
+}
+
+func BenchmarkGetPrices5(b *testing.B) {
+	benchmarkGetPrices(5, b)
+}
+
+func BenchmarkGetPrices10(b *testing.B) {
+	benchmarkGetPrices(10, b)
+}
+
+func BenchmarkGetPrices25(b *testing.B) {
+	benchmarkGetPrices(25, b)
+}
+
+func BenchmarkGetPrices50(b *testing.B) {
+	benchmarkGetPrices(50, b)
+}
+
+func BenchmarkGetPrices100(b *testing.B) {
+	benchmarkGetPrices(100, b)
 }
